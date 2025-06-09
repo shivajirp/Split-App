e0605e268bc6b7ab611b9db73dc33ef93f9d263f
shivajirp <shivajirp123@gmail.com>
2025-06-09 15:10:42 +0530

    project structure setup

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+// import expenseRoutes from "./routes/expenseRoutes.js";
+// import settlementRoutes from "./routes/settlementRoutes.js";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(express.json());
+// app.use("/expenses", expenseRoutes);
+// app.use("/", settlementRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
