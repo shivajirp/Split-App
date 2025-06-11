5040a98c9cdacb400dea32c324fca399c89fb4e2
shivajirp <shivajirp123@gmail.com>
2025-06-11 14:35:18 +0530

    commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 import expenseRoutes from "./routes/expenseRoutes.js";
 import settlementRoutes from "./routes/settlementRoutes.js";
@@ -8,6 +9,8 @@ import settlementRoutes from "./routes/settlementRoutes.js";
 dotenv.config();
 const app = express();
 connectDB();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/expenses", expenseRoutes);
