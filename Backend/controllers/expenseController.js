a7287668d2afc3e1e21eba5714d2cfc562083984
Shivaji Patil <shivaji.patil@devdynamics.ai>
2025-08-14 13:15:54 +0530

    remove commented-out validation for shared_with in addExpense function

--- Backend/controllers/expenseController.js
+++ Backend/controllers/expenseController.js
@@ -34,8 +34,8 @@ export const addExpense = async (req, res) => {
       amount <= 0 ||
       !description ||
       !paid_by ||
-      !Array.isArray(shared_with) ||
-      shared_with.length === 0
+      // !Array.isArray(shared_with) ||
+      // shared_with.length === 0
     ) {
       return res
         .status(400)
